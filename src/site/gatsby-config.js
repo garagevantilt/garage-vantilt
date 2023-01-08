d670a7715fd9731fac186a85c4f4d95e59828427
Maarten Louage <me@maartenlouage.nl>
2023-01-08 15:44:48 +0100

    Remove old images

--- src/site/gatsby-config.js
+++ src/site/gatsby-config.js
@@ -24,12 +24,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Garage Vantilt`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFF`,
+        theme_color: `#466071`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/apple-touch-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
